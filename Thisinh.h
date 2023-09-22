fde90409e353ffdce867dec6e4b02cf72be8b416
HTTTdh <130062550+HTTTdh@users.noreply.github.com>
2023-09-23 06:09:06 +0700

    Update Thisinh.h

--- Thisinh.h
+++ Thisinh.h
@@ -8,12 +8,20 @@ using namespace std;
 
 class ThiSinh : public Person {
     private:
-    double to;
+    string sbd;
+    double to,li,ho;
     public:
     ThiSinh();
-    ThiSinh( string name, string address, double to);
+    ThiSinh(string cccd, string name, Date date, string address, int gt, string sbd, double to, double li, double ho);
+    void setsbd(string sbd);
+    string getsbd();
     void setto(double to);
     double getto();
+    void setli(double li);
+    double getli();
+    void sethoa(double hoa);
+    double gethoa();
     void input();
+    void display();
 };
-#endif 
+#endif   
