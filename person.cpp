b0e24e940d32d6a840f62abe4e8c6056e6a0d257
HTTTdh <130062550+HTTTdh@users.noreply.github.com>
2023-09-23 06:10:36 +0700

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -4,10 +4,23 @@
 
 using namespace std;
 Person:: Person(){};
-Person::Person( string name, string address)
+Person::Person(string cccd, string name, Date date, string address, int gt)
 {
+    this->cccd = cccd;
     this->name = name;
+    this->date.day = date.day;
+    this->date.month = date.month;
+    this->date.year = date.year;
     this->address = address;
+    this->gt = gt;
+}
+void Person::setcccd(string cccd)
+{
+    this->cccd = cccd;
+}
+string Person::getcccd()
+{
+    return cccd;
 }
 void Person::setname(string name)
 {
@@ -17,6 +30,16 @@ string Person::getname()
 {
     return name;
 }
+void Person::setdate(Date date)
+{
+     this->date.day = date.day;
+    this->date.month = date.month;
+    this->date.year = date.year;
+}
+Date Person::getdate()
+{
+    return date;
+}
 void Person::setaddress(string address)
 {
     this->address =address;
@@ -25,11 +48,25 @@ string Person::getaddress()
 {
     return address;
 }
-void Person:: input()
+void Person::setgt(int gt)
 {
-    fflush(stdin);
-    cout << "ten: ";
-    getline(cin, name);
-    cout << "dia chi: ";
-    getline(cin, address);
+    this->gt=gt;
+}
+int Person::getgt()
+{
+    return gt;
+}
+void Person::nhapthongtin()
+{
+    cin.ignore(1);
+        cout << "nhap ho ten: ";
+        cin >> name;
+        cout << "nhap so cccd: ";
+        cin >> cccd;
+        cout << "nhap ngay thang nam sinh: ";
+        cin >> date.day >> date.month >> date.year ;
+        cout << "nhap dia chi (nhap tinh): ";
+        cin >> address;
+        cout << "nhap gioi tinh (0: Nam, 1: Nu): ";
+        cin >> gt;
 }
