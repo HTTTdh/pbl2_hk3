c77af473923b3e17e2eb45652d68276aeb70ade2
HTTTdh <130062550+HTTTdh@users.noreply.github.com>
2023-09-23 06:10:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
-#include"Thisinh.cpp"
-
+#include<string.h>
+#include"ThiSinh.cpp"
+#include"linklist.cpp"
 using namespace std;
 
 int main()
 {
-    
-    
+    LinkedList danhsach;
+    ThiSinh ts;
+    ts.input();
+    danhsach.insert(ts);
+    cout << "thong tin: ";
+    danhsach.display();
+    return 0;
 }
